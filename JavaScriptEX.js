322cba6be90087e3da0d8ab7f4ddd1c50011a788
PukoGit <138784881+PukoGit@users.noreply.github.com>
2024-11-05 18:01:48 +0800

    Initial commit

---
+++ JavaScriptEX.js
@@ -0,0 +1,13 @@
+var ToolEx = ToolEx || {};
+ToolEx.JavascriptEx = true;
+
+String.prototype.format = function (...args) {
+    return this.replace(/f{(\d+)}/g, (match, index) => args[index] || '%s');
+}
+
+
+
+Array.prototype.random = function () {
+    return this[Math.floor(Math.random() * this.length)];
+}
+
---
+++ TextManager.js
@@ -0,0 +1,33 @@
+class TextManager {
+    static #data = {};
+    static apply_func = [];
+    constructor() {
+        if (new.target === TextManager) {
+            throw new TypeError("Cannot construct TextManager instances.");
+        }
+    }
+    static load(land_path) {
+        return new Promise((resolve, reject) => {
+            fetch(land_path).then(response => response.text())
+                .then(text => {
+                    this.#data = JSON.parse(text);
+                    resolve();
+                })
+                .catch(error => reject(error));
+        });
+    }
+    static get(key) {
+        return this.#data[key] || key;
+    }
+    static apply() {
+        document.querySelectorAll('[data-text]').forEach(element => {
+            element.textContent = this.get(element.dataset.text);
+        });
+        this.apply_func.forEach(func => {
+            func();
+        });
+    }
+    static on_apply(func) {
+        this.apply_func.push(func);
+    }
+}
